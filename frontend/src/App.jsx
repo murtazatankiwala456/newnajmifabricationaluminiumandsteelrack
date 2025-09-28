a17c8868f7e0d4f857a00c707c69fa164ec155bf
Murtaza Tankiwala <115138620+murtazatankiwala456@users.noreply.github.com>
2025-09-28 09:33:37 +0530

    rename Pages to pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,5 @@
 //import AboutUsPage from "./components/Pages/About-Us/AboutUsPage";
-import ContactUsPage from "./components/Pages/Contact-Us/ContactUsPage";
+import ContactUsPage from "./components/pages/Contact-Us/ContactUsPage";
 //import ContactUs from "./components/Pages/Contact-us/ContactUs";
 
 // import HomePage from "./components/Pages/Home/HomePage.jsx";
